b10529ed819c8e5d7450bd721d5d4a522890c7a0
Vinicius Bastos Clemente <vclemente2@hotmail.com>
2023-03-25 23:02:35 -0300

    Atualização endpoint createEvent.

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -6,6 +6,10 @@ const createEvent = async (req, res) => {
     try {
         const eventCreated = await connection('events').insert({ ...event, company_id: user.id }).returning('*');
 
+        if (eventCreated.length === 0) {
+            return res.status(500).json({ message: 'Internal error. registration not completed' })
+        }
+
         return res.json(eventCreated[0]);
     } catch (error) {
         return res.status(500).json({ message: error.message });
